cbbfba36e63a21312d9963afa442305d6bfd9b5b
Bomin Kim <sirubom1203@gmail.com>
2025-05-02 17:23:11 +0900

    이미지 파일경로 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,29 +2,29 @@ import { CgBell } from "react-icons/cg";
 import { MdDarkMode, MdPerson, MdSearch } from "react-icons/md";
 
 const TeamList = [
-  { name: "KIA", img: "/public/KIA.png" },
-  { name: "삼성", img: "/public/Samsung.png" },
-  { name: "LG", img: "/public/LG.png" },
-  { name: "두산", img: "/public/두산.png" },
-  { name: "KT", img: "/public/KT.png" },
-  { name: "SSG", img: "/public/SSG.png" },
-  { name: "롯데", img: "/public/롯데.png" },
-  { name: "한화", img: "/public/한화.png" },
-  { name: "NC", img: "/public/NC.png" },
-  { name: "키움", img: "/public/키움.png" },
+  { name: "KIA", img: "/src/assets/images/team/kia.svg" },
+  { name: "삼성", img: "/src/assets/images/team/samsung.svg" },
+  { name: "LG", img: "/src/assets/images/team/lg.svg" },
+  { name: "두산", img: "/src/assets/images/team/doosan.svg" },
+  { name: "KT", img: "/src/assets/images/team/kt.svg" },
+  { name: "SSG", img: "/src/assets/images/team/ssg.svg" },
+  { name: "롯데", img: "/src/assets/images/team/lotte.svg" },
+  { name: "한화", img: "/src/assets/images/team/hanwha.svg" },
+  { name: "NC", img: "/src/assets/images/team/nc.svg" },
+  { name: "키움", img: "/src/assets/images/team/kiwoom.svg" },
 ];
 
 const liItemStyle = "flex cursor-pointer";
 const liImgStyle = "mr-3 h-7 w-7";
 const iconDiv = "w-[30px] h-[30px] bg-white rounded-2xl mt-6";
-const iconStyle = "w-5 h-5 ml-[5px] mt-1 text-[#002779]";
+const iconStyle = "w-5 h-5 ml-[5px] mt-1 text-[#002779] cursor-pointer";
 
 export default function Header() {
   return (
     <>
       <div className="z-50 fixed bg-[#0033A0] w-full h-[80px]">
         <div className="flex justify-between mx-10">
-          <img src="/public/로고.svg" className="h-12 w-72 ml-40 mt-3" />
+          <img src="/src/assets/images/로고.svg" className="h-12 w-72 ml-40 mt-3 cursor-pointer" />
           <div className="flex mr-40 gap-4">
             <div className={iconDiv}>
               <MdSearch className={iconStyle} />
