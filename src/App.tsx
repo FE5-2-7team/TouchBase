1852afd9f4be57101f824ff628a0a480150e3129
JUNHORANG <sjsk3358@naver.com>
2025-05-04 19:11:24 +0900

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import LoginPage from "./pages/LogInPage";
+import { Routes, Route } from "react-router";
+import RootLayout from "./layout/RootLayout";
+import Home from "./pages/Home";
+import ThreadsList from "./pages/ThreadsList";
+import Profile from "./components/Profile";
+import InboxMessage from "./components/InboxMessage";
 
 export default function App() {
   return (
     <>
-      <LoginPage></LoginPage>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/fanpage/:teamName" element={<ThreadsList />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/message" element={<InboxMessage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
