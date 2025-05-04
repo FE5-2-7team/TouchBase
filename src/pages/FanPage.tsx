3726cbc7dd22daf2e253b8c57494807bae9b85a4
JINJIY23 <dhkswksla22@naver.com>
2025-05-04 19:49:19 +0900

    Fan page 추가

---
+++ src/pages/FanPage.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import Upload from "../components/Upload";
+import Sidebar from "./../components/Sidebar";
+import ThreadsList from "./ThreadsList";
+
+export default function App() {
+  return (
+    <Router>
+      <div className="flex flex-col md:flex-row w-full max-w-[1500px] mx-auto">
+        <Sidebar />
+        <div className="flex-1 px-2 mt-[30px] md:mt-[50px] md:ml-20 md:mr-10">
+          <Upload />
+          <div className="my-[60px] border-t border-[#d9d9d9] w-full"></div>
+          <ThreadsList />
+          <div className="m-[30px]"></div>
+        </div>
+      </div>
+    </Router>
+  );
+}
