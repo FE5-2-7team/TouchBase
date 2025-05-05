04893f73e1cce40c1a0b42f97523a15f0f91fba7
JINJIY23 <dhkswksla22@naver.com>
2025-05-05 15:00:30 +0900

    팬 페이지 수정 및 삭제, 수정버튼 추가

--- src/pages/FanPage.tsx
+++ src/pages/FanPage.tsx
@@ -2,14 +2,14 @@ import Upload from "../components/Upload";
 import Sidebar from "./../components/Sidebar";
 import ThreadsList from "./../components/ThreadsList";
 
-export default function App() {
+export default function FanPage() {
   return (
     <>
       <div className="flex flex-col md:flex-row w-full max-w-[1500px] mx-auto">
         <Sidebar />
-        <div className="flex-1 px-2 mt-[30px] md:mt-[50px] md:ml-20 md:mr-10">
+        <div className="flex-1 px-2 mt-[250px] md:mt-[200px] md:ml-[50px] md:mr-10">
           <Upload />
-          <div className="my-[60px] border-t border-[#d9d9d9] w-full"></div>
+          <div className="my-[50px] border-t border-[#d9d9d9] w-full"></div>
           <ThreadsList />
           <div className="m-[30px]"></div>
         </div>
