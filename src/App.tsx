6fefba56ebeb39cca6a4fab185001d637eb45464
Bomin Kim <sirubom1203@gmail.com>
2025-05-05 19:00:00 +0900

    쪽지함 세부페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import RootLayout from "./layout/RootLayout";
 import Home from "./pages/Home";
 import FanPage from "./pages/FanPage";
 import Profile from "./components/Profile";
+import MessagePage from "./pages/MessagePage";
+import MessageContainer from "./components/MessageContainer";
 import InboxMessage from "./components/InboxMessage";
+import SentList from "./components/SentList";
 
 export default function App() {
   return (
@@ -13,7 +16,14 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/fanpage/:teamName" element={<FanPage />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/message" element={<InboxMessage />} />
+          <Route path="/message" element={<MessagePage />}>
+            <Route index element={<InboxMessage />} />
+            <Route path="inbox" element={<InboxMessage />} />
+            <Route path="sent" element={<SentList />} />
+            <Route path="sent/:id" element={<MessageContainer mode={"sent"} />} />
+            <Route path="write" element={<MessageContainer mode={"write"} />} />
+            <Route path="view/:id" element={<MessageContainer mode={"received"} />} />
+          </Route>
         </Route>
       </Routes>
     </>
