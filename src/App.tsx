f507d167524bcea45138c20b61d61d5d7b7da44b
JINJIY23 <dhkswksla22@naver.com>
2025-05-05 23:15:54 +0900

    fix 팬 페이지 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router";
 import RootLayout from "./layout/RootLayout";
 import Home from "./pages/Home";
-import ThreadsList from "./pages/ThreadsList";
+import ThreadsList from "./components/ThreadsList";
 import FanPage from "./pages/FanPage";
 import MessageContainer from "./components/MessageContainer";
 import SentList from "./components/SentList";
@@ -17,7 +17,7 @@ export default function App() {
         <Route path="/" element={<RootLayout />}>
           <Route path="/" element={<Home />} />
 
-          <Route path="/fanpage/:teamName" element={<ThreadsList />} />
+          <Route path="/fanpage/:teamName" element={<FanPage />} />
 
           <Route path="/profile" element={<ProfileHeader />}>
             <Route index path="posts" element={<ThreadsList />} />
