ffa46e030797a50e725b7122f8ac3382396de42c
YEONSEO <yeonseochoi0717@gmail.com>
2025-05-08 16:33:37 +0900

    feat: user 데이터로 렌더링

--- src/layout/ProfileLayout.tsx
+++ src/layout/ProfileLayout.tsx
@@ -1,26 +1,50 @@
 import { NavLink, Outlet, useParams } from "react-router";
 import profile from "../assets/images/bears.png";
 import mascot from "../assets/images/doosan_mascot.png";
+import { axiosInstance } from "../api/axiosInstance";
+import { useEffect, useState, useTransition } from "react";
+import { ExtendedUser } from "../types/postType";
 
 const USERNAME = "User name";
 const POSTNUM = 10;
 const FOLLOWERNUM = 200;
 const FOLLOWINGNUM = 100;
 const MESSAGENUM = 12;
+const USERID = "6811c3eeba84b946ffd47e45"; // params.id
 
 export default function ProfileLayout() {
+  const [user, setUser] = useState<ExtendedUser>();
+  const [isPending, startTransition] = useTransition();
   const params = useParams();
+
+  const getHandler = async () => {
+    try {
+      const { data } = await axiosInstance.get(`/users/${USERID}`);
+      console.log(data);
+      setUser(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  console.log(user);
+
+  useEffect(() => {
+    startTransition(async () => {
+      await getHandler();
+    });
+  }, []);
 
   return (
     <div className="flex flex-col gap-[34px] w-full max-w-[1200px] mx-auto mt-[40px]">
       <div className="border border-[#D9D9D9] shadow-md rounded-[10px] lg:h-[200px] md:h-[154px] sm:h-[120px] flex items-center gap-[50px] justify-between lg:px-[110px] px-[80px]">
         <img
           className="lg:w-[123px] lg:h-[123px] md:w-[100px] md:h-[100px] sm:w-[60px] sm:h-[60px]"
-          src={profile}
+          src={user?.image}
           alt="my profile"
         />
         <div className="">
-          <div className="md:text-[24px] font-bold sm:text-[10px]">{USERNAME}</div>
+          <div className="md:text-[24px] font-bold sm:text-[10px]">{user?.fullName}</div>
           <div className="flex lg:w-[580px] md:w-[420px] sm:w-[240px] justify-between mt-[7px] md:mb-[14px] sm:mb-[5px]">
             <NavLink
               to="posts"
@@ -28,7 +52,7 @@ export default function ProfileLayout() {
             >
               <button className="flex items-center cursor-pointer md:text-[16px] sm:text-[9px]">
                 <span className="md:text-[20px] sm:text-[9px] font-bold md:mr-[10px] sm:mr-[6px]">게시물</span>
-                {POSTNUM}
+                {user?.posts.length}
               </button>
             </NavLink>
             <NavLink
@@ -37,7 +61,7 @@ export default function ProfileLayout() {
             >
               <button className="flex items-center cursor-pointer md:text-[16px] sm:text-[9px]">
                 <span className="md:text-[20px] sm:text-[9px] font-bold md:mr-[10px] sm:mr-[6px]">팔로워</span>
-                {FOLLOWERNUM}
+                {user?.followers.length}
               </button>
             </NavLink>
             <NavLink
@@ -46,7 +70,7 @@ export default function ProfileLayout() {
             >
               <button className="flex items-center cursor-pointer md:text-[16px] sm:text-[9px]">
                 <span className="md:text-[20px] sm:text-[9px] font-bold md:mr-[10px] sm:mr-[6px]">팔로잉</span>
-                {FOLLOWINGNUM}
+                {user?.following.length}
               </button>
             </NavLink>
             <NavLink
@@ -55,7 +79,7 @@ export default function ProfileLayout() {
             >
               <button className="flex items-center cursor-pointer md:text-[16px] sm:text-[9px]">
                 <span className="md:text-[20px] sm:text-[9px] font-bold md:mr-[10px] sm:mr-[6px]">쪽지함</span>
-                {MESSAGENUM}
+                {user?.messages.length}
               </button>
             </NavLink>
           </div>
