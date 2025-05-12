8c5dcb08a0fda31894b1d1fca4010c3c5463c6a6
YEONSEO <yeonseochoi0717@gmail.com>
2025-05-12 17:47:54 +0900

    feat: 프로필 페이지 팔로우 카드 프로필 이미지 및 해당 유저 프로필 페이지로 이동 연결

--- src/components/Profile/FollowCard.tsx
+++ src/components/Profile/FollowCard.tsx
@@ -4,6 +4,7 @@ import useGetUser from "./useGetUser";
 import { ExtendedUser, Follow } from "../../types/postType";
 import { axiosInstance } from "../../api/axiosInstance";
 import { refreshStore } from "../../stores/refreshStore";
+import { Link } from "react-router";
 
 export default function FollowCard({ followId, profileId }: { followId: string; profileId: string }) {
   const userDetails: ExtendedUser | undefined = useGetUser(followId);
@@ -34,10 +35,15 @@ export default function FollowCard({ followId, profileId }: { followId: string; 
   };
 
   return (
-    <>
-      <div className="flex items-center border border-[#335CB3] dark:border-[#FFFFFF] rounded-[10px] w-[470px] h-[63px] justify-between px-[13px] my-[5px]">
+    <div className="flex items-center border border-[#335CB3] dark:border-[#FFFFFF] rounded-[10px] w-[470px] h-[63px] justify-between px-[13px] my-[5px]">
+      <Link to={`/profile/${userDetails?._id}/posts`} reloadDocument className="flex items-center">
         <div className="relative w-[34px] h-[34px]">
-          <FaUserCircle className="absolute w-full h-full fill-[#0033A0] dark:fill-[#FFFFFF]" />
+          {userDetails?.image ? (
+            <img src={userDetails?.image} alt="profile image" className="absolute w-full h-full rounded-full" />
+          ) : (
+            <FaUserCircle className="absolute w-full h-full fill-[#0033A0] dark:fill-[#FFFFFF]" />
+          )}
+
           <div
             className={twMerge(
               "absolute w-[8px] h-[8px] right-[1px] rounded-[100px] bg-[#00FF1E] dark:border dark:border-[#0033A0]",
@@ -45,28 +51,29 @@ export default function FollowCard({ followId, profileId }: { followId: string; 
             )}
           />
         </div>
-        <div className="text-[16px] text-[#6D6D6D] dark:text-[#FFFFFF] w-[170px]">
+        <div className="text-[16px] text-[#6D6D6D] dark:text-[#FFFFFF] w-[170px] ml-[10px]">
           {userDetails?.username ? userDetails?.username : userDetails?.fullName}
         </div>
-        <button className="w-[100px] h-[24px] text-[14px] rounded-[10px] bg-[#0033A0] text-[#ffffff] cursor-pointer">
-          쪽지 보내기
+      </Link>
+
+      <button className="w-[100px] h-[24px] text-[14px] rounded-[10px] bg-[#0033A0] text-[#ffffff] cursor-pointer">
+        쪽지 보내기
+      </button>
+      {following ? (
+        <button
+          className="w-[100px] h-[24px] text-[14px] rounded-[10px] bg-[#C5585F] text-[#ffffff] cursor-pointer"
+          onClick={unfollowHandler}
+        >
+          팔로우 취소
         </button>
-        {following ? (
-          <button
-            className="w-[100px] h-[24px] text-[14px] rounded-[10px] bg-[#C5585F] text-[#ffffff] cursor-pointer"
-            onClick={unfollowHandler}
-          >
-            팔로우 취소
-          </button>
-        ) : (
-          <button
-            className="w-[100px] h-[24px] text-[14px] rounded-[10px] bg-[#C5585F] text-[#ffffff] cursor-pointer"
-            onClick={followHandler}
-          >
-            팔로우
-          </button>
-        )}
-      </div>
-    </>
+      ) : (
+        <button
+          className="w-[100px] h-[24px] text-[14px] rounded-[10px] bg-[#C5585F] text-[#ffffff] cursor-pointer"
+          onClick={followHandler}
+        >
+          팔로우
+        </button>
+      )}
+    </div>
   );
 }
