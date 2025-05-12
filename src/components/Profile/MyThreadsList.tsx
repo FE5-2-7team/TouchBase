0c1dc48545c3207d2838643c41fa6fbe43e4ad18
YEONSEO <yeonseochoi0717@gmail.com>
2025-05-12 13:59:48 +0900

    feat: 프로필 페이지 content가 비었을 때 처리

--- src/components/Profile/MyThreadsList.tsx
+++ src/components/Profile/MyThreadsList.tsx
@@ -4,6 +4,7 @@ import { Post } from "../../types/postType";
 import Threads from "../FanPage/Threads";
 import { useParams } from "react-router";
 import { refreshStore } from "../../stores/refreshStore";
+import EmptyContent from "./EmptyContent";
 
 export default function MyThreadsList() {
   const [myPosts, setMyPosts] = useState<Post[]>([]);
@@ -31,37 +32,43 @@ export default function MyThreadsList() {
   return (
     <div className="flex flex-col gap-6 mb-[40px]">
       {/* 피드들 */}
-      {myPosts?.map((post) => {
-        let postTitle = "";
-        let postContent = "";
+      {myPosts.length > 0 ? (
+        myPosts?.map((post) => {
+          let postTitle = "";
+          let postContent = "";
 
-        try {
-          const parsedTitle = JSON.parse(post.title);
-          postTitle = parsedTitle[0].postTitle;
-          postContent = parsedTitle[0].postContent;
-        } catch (e) {
-          console.error("파싱실패", e);
-        }
+          try {
+            const parsedTitle = JSON.parse(post.title);
+            postTitle = parsedTitle[0].postTitle;
+            postContent = parsedTitle[0].postContent;
+          } catch (e) {
+            console.error("파싱실패", e);
+          }
 
-        const likeChecked = post.likes.some((like) => like.user === params.id);
+          const likeChecked = post.likes.some((like) => like.user === params.id);
 
-        return (
-          <Threads
-            key={post._id}
-            postId={post._id}
-            username={post.author?.username ?? post.author?.fullName}
-            postUserId={post.author._id}
-            title={postTitle}
-            content={postContent}
-            date={new Date(post.createdAt).toLocaleDateString()}
-            channel={post.channel.name}
-            images={post.image ? [post.image] : []}
-            likes={post.likes}
-            comments={post.comments}
-            likeChecked={likeChecked}
-          />
-        );
-      })}
+          return (
+            <Threads
+              key={post._id}
+              postId={post._id}
+              username={post.author?.username ?? post.author?.fullName}
+              postUserId={post.author._id}
+              title={postTitle}
+              content={postContent}
+              date={new Date(post.createdAt).toLocaleDateString()}
+              channel={post.channel.name}
+              images={post.image ? [post.image] : []}
+              likes={post.likes}
+              comments={post.comments}
+              likeChecked={likeChecked}
+            />
+          );
+        })
+      ) : (
+        <div className="h-[550px] border border-[#d9d9d9] shadow-md px-[27px] rounded-[10px] flex justify-center">
+          <EmptyContent message="게시물을 작성해보세요"></EmptyContent>
+        </div>
+      )}
     </div>
   );
 }
