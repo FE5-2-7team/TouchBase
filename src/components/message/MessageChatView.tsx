4650d38b244563c59d7da6730eeddc6b5c0e3903
Bomin Kim <sirubom1203@gmail.com>
2025-05-12 18:04:23 +0900

    쪽지 읽음처리 ui 수정

--- src/components/message/MessageChatView.tsx
+++ src/components/message/MessageChatView.tsx
@@ -20,34 +20,42 @@ export default function MessageChatView({ messages, myId }: Props) {
         ref={scrollRef}
         className="p-5 h-[540px] w-[70%] flex flex-col min-h-[540px] overflow-y-auto "
       >
-        {messages.map((msg, idx) => (
-          <div
-            key={idx}
-            className={`mb-5 flex ${msg.sender?._id === myId ? "justify-end " : "justify-start"}`}
-          >
-            <div
-              className={`px-4 py-3 rounded-xl w-fit max-w-[70%]"  
+        {messages.map((msg, idx) => {
+          const isMine = msg.sender?._id === myId;
+
+          return (
+            <div key={idx} className={`mb-5 flex ${isMine ? "justify-end " : "justify-start"}`}>
+              <div className="flex flex-col mr-2 items-end">
+                <div
+                  className={`px-4 py-3 rounded-xl max-w-[70%] w-auto"  
                 ${
                   msg.sender?._id === myId
-                    ? "bg-[#305AB3] text-white ml-20 break-words"
-                    : "bg-gray-100 text-gray-800 mr-20 break-words dark:bg-[#2D3037] dark:text-white"
+                    ? "bg-[#305AB3] text-white ml-20 break-words self-end"
+                    : "bg-gray-100 text-gray-800 mr-20 break-words dark:bg-[#2D3037] dark:text-white self-start"
                 }`}
-            >
-              <p className="text-lg">{msg.message}</p>
-              <p className="text-sm text-right mt-8 whitespace-normal opacity-50 dark:opacity-30">
-                {msg.createdAt
-                  ? new Date(msg.createdAt).toLocaleString("ko-KR", {
-                      month: "long",
-                      day: "numeric",
-                      hour: "2-digit",
-                      minute: "2-digit",
-                      hour12: true,
-                    })
-                  : ""}
-              </p>
+                >
+                  <p className="text-lg">{msg.message}</p>
+                  <p className="text-sm text-right mt-8 whitespace-normal opacity-50 dark:opacity-30">
+                    {msg.createdAt
+                      ? new Date(msg.createdAt).toLocaleString("ko-KR", {
+                          month: "long",
+                          day: "numeric",
+                          hour: "2-digit",
+                          minute: "2-digit",
+                          hour12: true,
+                        })
+                      : ""}
+                  </p>
+                </div>
+                {isMine && (
+                  <p className="text-xs text-right mt-1 opacity-60">
+                    {msg.seen ? "읽음" : "안읽음"}
+                  </p>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </>
   );
