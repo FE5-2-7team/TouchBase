16a5b7bb40e0afa1e29ab48d46ce87f7f34438bf
YEONSEO <yeonseochoi0717@gmail.com>
2025-05-14 10:34:39 +0900

    fix: 좋아요 유지 에러 해결

--- src/components/Profile/MyThreadsList.tsx
+++ src/components/Profile/MyThreadsList.tsx
@@ -5,12 +5,14 @@ import Threads from "../FanPage/Threads";
 import { useParams } from "react-router";
 import { refreshStore } from "../../stores/refreshStore";
 import EmptyContent from "./EmptyContent";
+import { userStore } from "../../stores/userStore";
 
 export default function MyThreadsList() {
   const [myPosts, setMyPosts] = useState<Post[]>([]);
   const [, startTransition] = useTransition();
   const params = useParams();
   const refresh = refreshStore((state) => state.refresh);
+  const loginUserId = userStore((state) => state.getUser()?._id);
 
   const getHandler = async () => {
     try {
@@ -45,7 +47,7 @@ export default function MyThreadsList() {
             console.error("파싱실패", e);
           }
 
-          const likeChecked = post.likes.some((like) => like.user === params.id);
+          const likeChecked = post.likes.some((like) => like.user === loginUserId);
 
           return (
             <Threads
