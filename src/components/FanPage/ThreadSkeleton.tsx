7e4b598d1ec19f261afa62e39ef50215068ce54b
JINJIY23 <dhkswksla22@naver.com>
2025-05-18 23:29:19 +0900

    스켈렛톤 수정

--- src/components/FanPage/ThreadSkeleton.tsx
+++ src/components/FanPage/ThreadSkeleton.tsx
@@ -1,17 +1,21 @@
 export default function ThreadSkeleton() {
   return (
-    <div
-      className="w-full p-4 shadow-md rounded-[10px] 
-      border border-[#d9d9d9] bg-white dark:bg-[#1e1e1e]"
-    >
-      <div className="flex items-center gap-4 mb-4">
-        <div className="w-10 h-10 bg-gray-300 dark:bg-gray-700 rounded-full" />
-        <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-1/3" />
+    <div className="shadow-md w-full max-w-full md:max-w-[1200px] mx-auto rounded-[10px] border border-[#d9d9d9]  dark:border-gray-700 flex flex-col">
+      <div className="p-[24px] flex gap-[25px]">
+        <div className="flex-shrink-0 self-start">
+          <div className="w-20 h-20 bg-gray-300 dark:bg-gray-700 rounded-full" />
+        </div>
+        <div className="flex flex-col w-full ml-[20px]">
+          <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-1/2 mb-4 mt-1" />
+          <div className="h-5 bg-gray-300 dark:bg-gray-700 rounded w-2/3 mb-2" />
+          <div className="flex items-center justify-between gap-4 mt-5">
+            <div className="flex items-center gap-7 mt-2">
+              <div className="w-[22px] h-[22px] bg-gray-200 rounded-full dark:bg-gray-700"></div>
+              <div className="w-[22px] h-[22px] bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="h-5 bg-gray-300 dark:bg-gray-700 rounded w-2/3 mb-2" />
-      <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-full mb-2" />
-      <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-5/6 mb-2" />
-      <div className="h-4 bg-gray-300 dark:bg-gray-700 rounded w-1/2" />
     </div>
   );
 }
