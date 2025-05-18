bb3ce16a5cbbbb93037a52929e004193e85974f8
ygvbhy <ygvbhy@naver.com>
2025-05-18 21:52:28 +0900

    style: 푸터 문구 수정 및 디자인 수정

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -6,30 +6,24 @@ export default function Footer() {
       <footer className="bg-[#0D1B2A] dark:bg-[#16171B]">
         <div className="container px-4 md:px-0 flex justify-start items-center mx-auto h-[200px] text-white gap-10">
           <div className="hidden md:flex items-center">
-            <img
-              src={logo}
-              alt="모바일버전 로고"
-              className="mt-[6%] ml-4 h-10 w-fit"
-            />
+            <img src={logo} alt="로고" className="mt-[6%] ml-4 h-10 w-fit" />
           </div>
           <div className="flex flex-col gap-4">
             <div className="flex gap-4">
               <div className="cursor-pointer hover:text-[#FF9500]">
-                개인 정보 처리 방침
-              </div>
-              |
-              <div className="cursor-pointer hover:text-[#FF9500]">
-                고객 문의
-              </div>
-              |
-              <div className="cursor-pointer hover:text-[#FF9500]">
-                사이트 맵
+                <a
+                  href="https://github.com/FE5-2-7team/TouchBase"
+                  target="_blank"
+                >
+                  GitHub
+                </a>
               </div>
             </div>
             <div>
-              (사)한국야구위원회 | 서울시 강남구 강남대로 278 | 02)3460-4600
+              프로그래머스 부트캠프 5기 7팀 터치베이스 - 박상윤, 김보민, 이준호,
+              정지유, 최연서
               <br />
-              Copyrightⓒ KBO, All Rights Reserved.
+              Copyrightⓒ 2025 TouchBase, All Rights Reserved.
             </div>
           </div>
         </div>
