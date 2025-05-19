653bec04847f77604b1c31fff00fdd37deb5c093
ygvbhy <ygvbhy@naver.com>
2025-05-19 09:43:07 +0900

    Fix: API 링크 수정

--- src/api/notification.ts
+++ src/api/notification.ts
@@ -9,7 +9,7 @@ interface NotificationType {
 
 const getNotifications = async () => {
   try {
-    const response = await axiosInstance.get("/notifications");
+    const response = await axiosInstance.get("/notifications/ㅊ");
     return response.data;
   } catch (error) {
     console.error("알림 목록 불러오기 실패", error);
@@ -18,7 +18,10 @@ const getNotifications = async () => {
 
 const createNotification = async (notification: NotificationType) => {
   try {
-    const response = await axiosInstance.post("/notifications", notification);
+    const response = await axiosInstance.post(
+      "/notifications/create",
+      notification
+    );
     return response.data;
   } catch (error) {
     console.error("알림 생성 실패", error);
