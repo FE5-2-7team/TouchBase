5717c8e6f106c209404b64e0aaeb1818b215afbf
ygvbhy <ygvbhy@naver.com>
2025-05-19 09:44:24 +0900

    Fix: 오타 수정

--- src/api/notification.ts
+++ src/api/notification.ts
@@ -9,7 +9,7 @@ interface NotificationType {
 
 const getNotifications = async () => {
   try {
-    const response = await axiosInstance.get("/notifications/ㅊ");
+    const response = await axiosInstance.get("/notifications");
     return response.data;
   } catch (error) {
     console.error("알림 목록 불러오기 실패", error);
